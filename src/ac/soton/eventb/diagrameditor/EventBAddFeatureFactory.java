60fb2cd3a4e97712d758461009a803b3fd62397c
Elliot Hughes <elliot.hughes@gmail.com>
2013-12-03 18:25:11 +0000

    Remove unneeded factory

--- src/ac/soton/eventb/diagrameditor/EventBAddFeatureFactory.java
+++
@@ -1,8 +0,0 @@
-package ac.soton.eventb.diagrameditor;
-
-import org.eclipse.graphiti.features.IAddFeature;
-import org.eclipse.graphiti.features.context.IAddContext;
-
-public class EventBAddFeatureFactory extends
-		EventBFeatureFactory<IAddContext, IAddFeature> {
-}
